b4968ce0787010341e7adcfa0a0c4ccf402483b6
Davide <milazzodavide@live.com>
2026-07-30 22:24:37 +0200

    Added base files

---
+++ src/test/java/milazzodavide/panda/PandaApplicationTests.java
@@ -0,0 +1,13 @@
+package milazzodavide.panda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
